c8d5234e76323a6453840a78369fbf2d59c313bb
Dirk Grunwald <grunwald@colorado.edu>
2023-09-13 20:56:54 +0000

    add port

--- server/start-server-docker.sh
+++ server/start-server-docker.sh
@@ -12,6 +12,6 @@ docker run -d --name nats-main \
        -v /home/ubuntu/nats:/home/ubuntu/nats \
        -v ./config-cluster.json:/etc/nats/nats-server.conf \
        -e TOKEN=$TOKEN -e SERVER=$SERVER \
-       -p 4222:4222 -p 6222:6222 -p 8222:8222 \
+       -p 4222:4222 -p 6222:6222 -p 8222:8222 -p 8080:8080 \
        nats -c /etc/nats/nats-server.conf 
 
