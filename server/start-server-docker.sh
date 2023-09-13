2409eafe9a0229a06d2ee17842dc83bfd277d802
Dirk Grunwald <grunwald@colorado.edu>
2023-09-13 20:49:56 +0000

    use docker version

---
+++ server/start-server-docker.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ -z $TOKEN ];
+then
+    echo You need to define TOKEN
+    exit 100
+fi
+
+export SERVER=$(hostname)
+
+docker run -d --name nats-main \
+       -v /home/ubuntu/nats:/home/ubuntu/nats \
+       -v ./config-cluster.json:/etc/nats/nats-server.conf \
+       -e TOKEN=$TOKEN -e SERVER=$SERVER \
+       -p 4222:4222 -p 6222:6222 -p 8222:8222 \
+       nats -c /etc/nats/nats-server.conf 
+
